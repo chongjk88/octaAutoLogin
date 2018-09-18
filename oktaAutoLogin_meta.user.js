3384d22ab6a274574161e7bacad217e28abc42ff
Jun Kim <jun.kim@cdk.com>
2018-09-18 12:57:09 -0700

    version

--- oktaAutoLogin_meta.user.js
+++ oktaAutoLogin_meta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Okta Auto Login
-// @version      0.4
+// @version      0.5
 // @author       cire.pt, jk
 // @match        https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx
 // @match        https://connectcdk.okta.com/app/UserHome
