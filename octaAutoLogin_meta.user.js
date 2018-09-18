bda62fa921e4bb4dcc1377bddf83b4efadb75352
Jun Kim <jun.kim@cdk.com>
2018-09-18 12:00:55 -0700

    fix path

--- octaAutoLogin_meta.user.js
+++ octaAutoLogin_meta.user.js
@@ -5,7 +5,7 @@
 // @match        https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx
 // @match        https://connectcdk.okta.com/app/UserHome
 // @match        https://portal.cobalt.com/
-// @downloadURL  https://raw.githubusercontent.com/chongjk88/octaAutoLogin/master/defaultState_meta.user.js
-// @require      https://raw.githubusercontent.com/chongjk88/octaAutoLogin/master/defaultState.js
+// @downloadURL  https://raw.githubusercontent.com/chongjk88/octaAutoLogin/master/octaAutoLogin_meta.user.js
+// @require      https://raw.githubusercontent.com/chongjk88/octaAutoLogin/master/octaAutoLogin.js
 // @grant        none
 // ==/UserScript==
