ee9f3b2501664a709b28cd9675fac363bdcf89e1
Jun Kim <jun.kim@cdk.com>
2018-09-18 12:55:46 -0700

    fix for ff

--- oktaAutoLogin.js
+++ oktaAutoLogin.js
@@ -13,13 +13,10 @@ if (curURL === "https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx") {
   };
 } else if (curURL === "https://connectcdk.okta.com/app/UserHome") {
   // auto select DCC
-  window.onload = () => {
-    setTimeout(function() {
-      const DCCLink = document.querySelector("a[data-se='app-button-0oa7jc6iwxXAtbpGb1t7']");
-      DCCLink.target = "_self";
-      DCCLink.click();
-    }, 2000);
-  };
+  setTimeout(function() {
+    const DCCLink = document.querySelector("a[data-se='app-button-0oa7jc6iwxXAtbpGb1t7']");
+    window.open(DCCLink.getAttribute("href"),"_self");
+  }, 1000);
 } else if (curURL === "https://portal.cobalt.com/") {
   // auto choose WSM
   window.onload = () => {
