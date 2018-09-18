b5cfc78b3ac8b96259024cc42036802a19304baf
Jun Kim <jun.kim@cdk.com>
2018-09-18 13:25:24 -0700

    c

--- oktaAutoLogin.js
+++ oktaAutoLogin.js
@@ -2,7 +2,6 @@ const curURL = window.location.href;
 
 if (curURL === "https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx") {
   // auto select Okta (Prod)
-  window.onload = () => {
     if (document.getElementById("idp_ThisRpRadioButton").checked === true) {
       document.getElementById("idp_OtherRpRadioButton").checked = true;
       document.getElementById("idp_ThisRpRadioButton").checked = false;
@@ -10,7 +9,6 @@ if (curURL === "https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx") {
     }
     document.getElementById("idp_RelyingPartyDropDownList").querySelector("option[value='ae05ef51-b11c-e811-80f3-0050569214ee']").selected = true;
     document.getElementById("idp_GoButton").click();
-  };
 } else if (curURL === "https://connectcdk.okta.com/app/UserHome") {
   // auto select DCC
   setTimeout(function() {
--- oktaAutoLogin_meta.user.js
+++ oktaAutoLogin_meta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Okta Auto Login
-// @version      0.7
+// @version      0.8
 // @author       cire.pt, jk
 // @match        https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx
 // @match        https://connectcdk.okta.com/app/UserHome
