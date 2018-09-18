775c83e97f90777610b1f0be6d7964b40e17be09
Jun Kim <jun.kim@cdk.com>
2018-09-18 12:00:32 -0700

    update

---
+++ octaAutoLogin_meta.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Octa Auto Login
+// @version      0.1
+// @author       cire.pt, jk
+// @match        https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx
+// @match        https://connectcdk.okta.com/app/UserHome
+// @match        https://portal.cobalt.com/
+// @downloadURL  https://raw.githubusercontent.com/chongjk88/octaAutoLogin/master/defaultState_meta.user.js
+// @require      https://raw.githubusercontent.com/chongjk88/octaAutoLogin/master/defaultState.js
+// @grant        none
+// ==/UserScript==
