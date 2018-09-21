afaf12e3f4c16e93fa008e13213a536808b150e4
Jun Kim <jun.kim@cdk.com>
2018-09-21 09:20:52 -0700

    goes to edit tab

--- oktaAutoLogin.js
+++ oktaAutoLogin.js
@@ -9,5 +9,5 @@ if (curURL === "https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx") {
   document.getElementById("idp_RelyingPartyDropDownList").querySelector("option[value='ae05ef51-b11c-e811-80f3-0050569214ee']").selected = true;
   document.getElementById("idp_GoButton").click();
 } else if (curURL === "https://connectcdk.okta.com/app/UserHome") {
-  window.open("http://websites.cobalt.com/wsm/","_self");
+  window.open("http://websites.cobalt.com/wsm/editSite.do","_self");
 }
--- oktaAutoLogin_meta.user.js
+++ oktaAutoLogin_meta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Okta Auto Login
-// @version      1.0
+// @version      1.1
 // @author       cire.pt, jk
 // @match        https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx
 // @match        https://connectcdk.okta.com/app/UserHome
