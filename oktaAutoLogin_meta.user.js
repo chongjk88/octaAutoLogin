8424d10aa88cf55cf84e2748196c19ba213b642b
Jun Kim <jun.kim@cdk.com>
2018-10-26 15:36:25 -0700

    bug fix

--- oktaAutoLogin_meta.user.js
+++ oktaAutoLogin_meta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Okta Auto Login
-// @version      1.2
+// @version      1.3
 // @author       cire.pt, jk
 // @match        https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx
 // @match        https://connectcdk.okta.com/app/UserHome
