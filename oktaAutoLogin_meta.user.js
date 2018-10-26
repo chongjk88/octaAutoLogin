6649ab88f6b29705f5b06e8c50cea6aa9181f335
Jun Kim <jun.kim@cdk.com>
2018-10-26 15:41:22 -0700

    raw link update

--- oktaAutoLogin_meta.user.js
+++ oktaAutoLogin_meta.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Okta Auto Login
-// @version      1.3
+// @version      1.4
 // @author       cire.pt, jk
 // @match        https://sso.cdk.com/adfs/ls/idpinitiatedsignon.aspx
 // @match        https://connectcdk.okta.com/app/UserHome
 // @match        https://portal.cobalt.com/
-// @downloadURL  https://raw.githubusercontent.com/chongjk88/oktaAutoLogin/master/oktaAutoLogin_meta.user.js
-// @require      https://raw.githubusercontent.com/chongjk88/oktaAutoLogin/master/oktaAutoLogin.js
+// @downloadURL  https://github.com/chongjk88/oktaAutoLogin/raw/master/oktaAutoLogin_meta.user.js
+// @require      https://github.com/chongjk88/oktaAutoLogin/raw/master/oktaAutoLogin.js
 // @grant        none
 // ==/UserScript==
